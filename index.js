b6aae2f2fa15930e41f9a2067b9aebd915f27682
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-10-03 21:25:30 +0300

    Fix syntax err

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function(url, series, opts) {
         function gotMilk(url) {
 
             got(url, {
-                json: true
+                json: true,
                 ...opts
             }).then(res => {
                 const link = parseLink(res.headers.link);
